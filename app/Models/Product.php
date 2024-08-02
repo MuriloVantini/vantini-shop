98ae0957b829772b200d92d91d79442336c88c83
murilovantini <m.henrique.vantini18@gmail.com>
2024-08-02 10:48:52 -0300

    feat: adicionar relacionamento many to many entre categoria e produto

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'description', 'price'];
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class, 'category_product');
+    }
 }
